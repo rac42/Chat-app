01d4ffc61479d34d421b0f016af4a2e10f7f2779
rac42 <rachit0942@gmail.com>
2025-01-12 17:19:17 +0530

    message model and controller done

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import {connectDB} from "./lib/db.js";
 import cookieParser from"cookie-parser";
-
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connect } from "mongoose";
 
 const app = express();
@@ -14,6 +14,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
+
+app.use("/api/message", messageRoutes)
 
 app.listen(PORT, ()=> {
     console.log(`server is running on ${PORT}`);
