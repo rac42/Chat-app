0d3c7c9bcb85476e9a627b62a2ff0e176339062e
rac42 <rachit0942@gmail.com>
2025-01-13 00:40:04 +0530

    started the fronted part and made the login and signup pages

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import {connectDB} from "./lib/db.js";
 import cookieParser from"cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
 import { connect } from "mongoose";
 
 const app = express();
@@ -12,6 +13,10 @@ dotenv.config()
 const PORT = process.env.PORT
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 app.use("/api/auth", authRoutes)
 
